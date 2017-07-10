7a89361554bd8a167986b33ffede9c8d8b2dba03
Joshua Brot <jbrot@umich.edu>
2017-07-09 23:38:20 -0500

    Added some primitive command line handling. We now can input files and then dump their contents to the shell. Up next: better error checking and then onto the preprocessor.
    Signed-off-by: Joshua Brot <jbrot@umich.edu>

---
+++ src/Config.hs
@@ -0,0 +1,47 @@
+module Config where
+
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Reader
+
+import Data.Semigroup
+import Data.Text (Text)
+
+import Options.Applicative
+
+-- This structure contains the general configuration options describing how
+-- the compilation should take place
+data Config = Config
+
+newtype Env m a = Env (ReaderT Config m a)
+
+instance Functor m => Functor (Env m) where
+    fmap f (Env a) = Env (fmap f a)
+instance Applicative m => Applicative (Env m) where
+    pure = Env . pure
+    Env a <*> Env b = Env (a <*> b)
+instance Monad m => Monad (Env m) where
+    Env a >>= f = Env $ (\(Env x) -> x) . f =<< a
+
+instance MonadIO m => MonadIO (Env m) where
+    liftIO = Env . liftIO
+
+runEnv :: Env m a -> Config -> m a
+runEnv (Env r) a = runReaderT r a
+
+class Monad m => HasEnv m where
+    env :: m Config
+instance Monad m => HasEnv (Env m) where
+    env = Env ask
+
+configParser :: Parser Config
+configParser = pure Config
+
+cmdlnParser :: Parser (Config, [String])
+cmdlnParser = (,) <$> configParser
+                  <*> some (strArgument $ metavar "FILES ...")
+
+cmdlnParserInfo :: ParserInfo (Config, [String])
+cmdlnParserInfo = info (cmdlnParser <**> helper)
+   ( fullDesc
+  <> header "cch - a toy c compiler"
+   )
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+
+import Config
+
+import Control.Monad.IO.Class
+
+import Data.Semigroup
+import Data.Text (Text)
+import qualified Data.Text.IO as T
+
+import Options.Applicative
+
+compile :: (MonadIO m, HasEnv m) => String -> m ()
+compile f = do
+    env
+    liftIO . putStrLn $ "Compiling: " <> f
+    contents <- liftIO . T.readFile $ f
+    liftIO . T.putStrLn $ "Contents: \n" <> contents
+    return ()
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    (conf, trgts) <- execParser cmdlnParserInfo
+    runEnv (mapM compile trgts) conf
