7b14f5df47c4533bacaadd2ac92b933ca8cc62e9
Joshua Brot <jbrot@umich.edu>
2017-07-12 00:02:31 -0500

    The initial pass is now done with parsec and includes location annotations. The code is a mess, so I'll clean this up tommorrow.
    Signed-off-by: Joshua Brot <jbrot@umich.edu>

--- src/Main.hs
+++ src/Main.hs
@@ -1,41 +1,64 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts, OverloadedStrings #-}
 module Main where
 
 import Config
 
-import Control.Exception
+import Control.Exception (tryJust)
 import Control.Monad
 import Control.Monad.IO.Class
 
+import Data.Foldable (asum)
 import Data.Semigroup
-import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
 
-import Options.Applicative
+import Options.Applicative (execParser)
 
 import System.IO.Error
 
-resolveTriglyphs :: Text -> Text
-resolveTriglyphs s = foldr (uncurry T.replace) s
-    [ ("??=", "#"),  ("??)", "]"), ("??!", "|")
-    , ("??(", "["),  ("??'", "^"), ("??>", "}")
-    , ("??/", "\\"), ("??<", "{"), ("??-", "~")
-    ]
+import Text.Parsec
+
+preplace :: Stream s m Char => String -> String -> ParsecT s u m String
+preplace f r = const r <$> string f
+
+triglyphs :: [(String, String)]
+triglyphs = [ ("??=", "#"),  ("??)", "]"), ("??!", "|")
+            , ("??(", "["),  ("??'", "^"), ("??>", "}")
+            , ("??/", "\\"), ("??<", "{"), ("??-", "~")
+            , ("\\\n", "") -- Not technically a triglyph
+            ]
+
+-- The first pass of the source file. This resolves triglyphs and
+-- escaped newlines while also annotating the original positions.
+firstPass :: SourceName -> String -> Either ParseError [(SourcePos, Char)]
+firstPass = parse $ fmap mconcat $
+    many (fmap sequence $ (,) <$> getPosition <*> (asum (try . uncurry preplace <$> triglyphs) <|> (fmap pure anyChar)))
+
+--resolveTriglyphs :: Text -> Text
+--resolveTriglyphs s = foldr (uncurry T.replace) s
+--    [ ("??=", "#"),  ("??)", "]"), ("??!", "|")
+--    , ("??(", "["),  ("??'", "^"), ("??>", "}")
+--    , ("??/", "\\"), ("??<", "{"), ("??-", "~")
+--    ]
+
+-- This function eliminates any new line characters immediately preceded by a
+-- backslash. Said backslash is also eliminated.
+--combineLines :: Text -> Text
+--combineLines = T.replace "\\\n" ""
 
 compile :: (MonadIO m, MonadPlus m, HasEnv m) => String -> m ()
 compile f = do
     env
     liftIO . putStrLn $ "Compiling: " <> f
     let isDNE e = if isDoesNotExistError e then Just e else Nothing
-    res <- liftIO . tryJust isDNE $ T.readFile f
+    res <- liftIO . tryJust isDNE $ readFile f
     case res of
         (Left e) -> do
             liftIO . putStrLn $ "File not found: " <> f
             return ()
         (Right t) -> do
-            let contents = resolveTriglyphs t
-            liftIO . T.putStrLn $ "Contents: \n" <> contents
+            let contents = firstPass f t
+            liftIO . putStrLn $ "Contents: \n" 
+            liftIO . print $ contents
+            liftIO . print $ fmap (fmap snd) contents
             return ()
 
 main :: IO ()
